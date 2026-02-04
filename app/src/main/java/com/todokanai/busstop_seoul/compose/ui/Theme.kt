6aedb739131eeda3ea182d58f7500979d9f920e1
Todokanai <bh951201@gmail.com>
2026-02-04 15:54:43 +0900

    다크 모드에서 텍스트 색이 검은색으로 나오는 현상 fix
    https://stackoverflow.com/a/68770041/19302653

--- app/src/main/java/com/todokanai/busstop_seoul/compose/ui/Theme.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/ui/Theme.kt
@@ -1,9 +1,12 @@
 package com.todokanai.busstop_seoul.compose.ui
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
 private val LightColors = lightColorScheme(
@@ -20,11 +23,22 @@ private val LightColors = lightColorScheme(
 private val DarkColors = darkColorScheme()
 
 @Composable
-fun BusStopTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colorScheme = LightColors,
-        typography = MaterialTheme.typography, // 기본값 사용
-        shapes = MaterialTheme.shapes,         // 기본값 사용
-        content = content
-    )
+fun BusStopTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    CompositionLocalProvider(
+        LocalContentColor provides
+                if(darkTheme) MaterialTheme.colorScheme.background
+                else MaterialTheme.colorScheme.primary
+    ) {
+
+        MaterialTheme(
+            //colorScheme = LightColors,
+            typography = MaterialTheme.typography, // 기본값 사용
+            shapes = MaterialTheme.shapes,         // 기본값 사용
+            content = content
+        )
+    }
 }
