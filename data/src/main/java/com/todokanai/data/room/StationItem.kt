4b4a3bc54bc916b6104a6efd80cd4198a1897f55
Todokanai <bh951201@gmail.com>
2026-02-25 07:24:58 +0900

    code refactor

--- data/src/main/java/com/todokanai/data/room/StationItem.kt
+++ data/src/main/java/com/todokanai/data/room/StationItem.kt
@@ -7,9 +7,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "stationItem")
 data class StationItem(
     @PrimaryKey             // Todo: PrimaryKey 선택이 적절한지 확인 필요
-    @ColumnInfo var stId: String,          // 정류소 ID
+    @ColumnInfo var stId: Long,          // 정류소 ID
     @ColumnInfo var stNm: String,          // 정류소 명
-    @ColumnInfo var arsId: String,         // 정류소 고유 번호 (5자리)
+    @ColumnInfo var arsId: Long,         // 정류소 고유 번호 (5자리)
     @ColumnInfo var tmX: Double,           // 경도 (Longitude)
     @ColumnInfo var tmY: Double,           // 위도 (Latitude)
     @ColumnInfo var posX: String? = null,          // 좌표 X (GRS80)
