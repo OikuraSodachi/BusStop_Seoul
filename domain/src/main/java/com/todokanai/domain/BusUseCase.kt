1c5de17527f42f9d4901b6b7576f5b3da1cfcc28
Todokanai <bh951201@gmail.com>
2026-02-27 07:17:27 +0900

    retrofit 호출 후 room 에 저장 로직 추가

--- domain/src/main/java/com/todokanai/domain/BusUseCase.kt
+++ domain/src/main/java/com/todokanai/domain/BusUseCase.kt
@@ -18,12 +18,21 @@ class BusUseCase @Inject constructor(
         return stationRepository.getStationArriveInfos(arsId)
     }
 
+    // Todo: insert 작업 완료 후에 return 하는 것이 바람직한 방향인지?
     suspend fun getStationByName(stNm:String):List<StationItem>{
-        return stationRepository.getStationByName(stNm)
+        val response = stationRepository.getStationByName(stNm)
+        response.forEach {
+            localDataRepository.insertStation(it)
+        }
+        return response
     }
 
     suspend fun getStationByPosition(tmX: Double, tmY: Double, radius:Int):List<StationItem>{
-        return stationRepository.getStationByPosition(tmX.toString(), tmY.toString(), radius.toString())
+        val response = stationRepository.getStationByPosition(tmX.toString(), tmY.toString(), radius.toString())
+        response.forEach {
+            localDataRepository.insertStation(it)
+        }
+        return response
     }
 
     suspend fun getBusPositions(routeId: Long): List<BusPositionItem> {
