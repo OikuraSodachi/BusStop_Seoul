0408d5a61955931d42f79b42dc60edb7dbbd28d5
Todokanai <bh951201@gmail.com>
2026-03-09 13:57:46 +0900

    TextAlign 추가

--- app/src/main/java/com/todokanai/busstop_seoul/compose/holder/LineInfoHolder.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/holder/LineInfoHolder.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.todokanai.busstop_seoul.dataclass.LineInfo
@@ -40,7 +41,8 @@ fun LineInfoHolder(
                 text = lineInfo.stNm,
                 modifier = Modifier
                     .wrapContentSize()
-                    .weight(1f)
+                    .weight(1f),
+                textAlign = TextAlign.Center
             )
 
             Box(
