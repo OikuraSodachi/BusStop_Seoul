ed04bc59351d45ea50233a287b19415d7f20796d
Todokanai <bh951201@gmail.com>
2026-03-22 13:12:12 +0900

    Todo: commit 528123f8 에서 textField 렉 없이 작동 확인됨

--- app/src/main/java/com/todokanai/busstop_seoul/compose/tab/SearchTab.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/tab/SearchTab.kt
@@ -31,7 +31,7 @@ fun SearchTab(
     saveToFavorites:(SearchResult)->Unit
 ){
     var text by remember { mutableStateOf("") }
-
+    // Todo: commit 528123f8 에서 textField 렉 없이 작동 확인됨
     Column{
         Row(modifier = Modifier.fillMaxWidth()) {
             TextField(
