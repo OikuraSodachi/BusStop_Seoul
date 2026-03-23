d34d44c801b599f88c3248731b004330599f97db
Todokanai <bh951201@gmail.com>
2026-03-23 09:40:39 +0900

    전체 노선, 정류소 정보를 csv 에서 최초 1회만 가져오도록 변경. Todo: commit 277dcd04 의 issue ( TextField 입력시 렉 ) 해결된듯?

--- domain/src/main/java/com/todokanai/domain/BusUseCase.kt
+++ domain/src/main/java/com/todokanai/domain/BusUseCase.kt
@@ -4,7 +4,10 @@ import com.todokanai.domain.dataclass.ArriveInfoByRouteAllItem
 import com.todokanai.domain.dataclass.BusLineItem
 import com.todokanai.domain.dataclass.StationArriveItem
 import com.todokanai.domain.dataclass.StationItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class BusUseCase @Inject constructor(
@@ -13,13 +16,23 @@ class BusUseCase @Inject constructor(
     private val localDataRepository: LocalDataRepository
 ) {
 
+    var allStations = mutableListOf<StationItem>()
+    var allBusLines = mutableListOf<BusLineItem>()
+
+    init{
+        CoroutineScope(Dispatchers.IO).launch {
+            allStations.addAll(localDataRepository.getAllStationItems())
+            allBusLines.addAll(localDataRepository.getAllBusLineItems())
+        }
+    }
+
     suspend fun getArriveInfos(arsId:Long):List<StationArriveItem>{
         return stationRepository.getStationArriveInfos(arsId)
     }
 
     suspend fun getStationByName(stNm:String):List<StationItem>{
         val result = mutableListOf<StationItem>()
-        val allStations = localDataRepository.getAllStationItems()
+        val allStations = allStations
         allStations.forEach {
             if(it.stNm.contains(stNm)){
                 result.add(it)
@@ -38,7 +51,7 @@ class BusUseCase @Inject constructor(
 
     suspend fun getLineInfosFromKeyWord(keyWord:String):List<BusLineItem>{
         val result = mutableListOf<BusLineItem>()
-        val lineList = localDataRepository.getAllBusLineItems()  // 전체 노선 정보 가져오기
+        val lineList = allBusLines  // 전체 노선 정보 가져오기
 
         lineList.forEach {
             if(it.rtNm.contains(keyWord)){
