efd822c6e8b1656907bb25ae079ee7a40186296e
Todokanai <bh951201@gmail.com>
2026-04-08 07:31:11 +0900

    remove unused import

--- app/src/main/java/com/todokanai/busstop_seoul/compose/MainScreen.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/MainScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
