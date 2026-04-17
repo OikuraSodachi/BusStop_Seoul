7bb67e80d387af2566841271a55c652a60a24cb9
Todokanai <bh951201@gmail.com>
2026-04-18 07:23:41 +0900

    동적 Size 계산 보류 RangeSelectionMode 작성중

--- app/src/main/java/com/todokanai/busstop_seoul/compose/buttons/MainMenuButton.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/buttons/MainMenuButton.kt
@@ -3,10 +3,7 @@ package com.todokanai.busstop_seoul.compose.buttons
 import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntSize
 import com.todokanai.busstop_seoul.R
 
 @Composable
