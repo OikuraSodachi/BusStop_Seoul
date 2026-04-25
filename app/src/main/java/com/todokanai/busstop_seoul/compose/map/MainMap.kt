388ba29fd25cff94231768883d72af593a699dbd
Todokanai <bh951201@gmail.com>
2026-04-25 15:35:08 +0900

    startGroup, endGroup marker 색상 작동 확인됨

--- app/src/main/java/com/todokanai/busstop_seoul/compose/map/MainMap.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/map/MainMap.kt
@@ -2,7 +2,12 @@ package com.todokanai.busstop_seoul.compose.map
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
@@ -21,6 +26,22 @@ fun MainMap(
     mainMapCallback: MainMapInterface
 ){
 
+    var startGroup by remember{ mutableStateOf(emptyList<Long>())}
+    var endGroup by remember{ mutableStateOf(emptyList<Long>())}
+
+    /**
+     * @param stId 정류소 ID
+     * @return color for the marker **/
+    fun markerColorSelector(stId:Long):Float{
+        return if(startGroup.contains(stId)){
+            BitmapDescriptorFactory.HUE_GREEN
+        }else if(endGroup.contains(stId)){
+            BitmapDescriptorFactory.HUE_BLUE
+        }else {
+            BitmapDescriptorFactory.HUE_RED
+        }
+    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
@@ -38,8 +59,10 @@ fun MainMap(
                 state = MarkerState(position = markerInfo.position),
                 title = markerInfo.title,
                 snippet = markerInfo.snippet,
+                icon = BitmapDescriptorFactory.defaultMarker(markerColorSelector(markerInfo.stationInfo.stId)),
                 onClick = {
                     mainMapCallback.onMarkerClick(markerInfo)
+                    println(markerInfo.stationInfo.stId)
                     false
                 }
 
