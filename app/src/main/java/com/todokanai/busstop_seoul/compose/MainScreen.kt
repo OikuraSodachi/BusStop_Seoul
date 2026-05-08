0a9bd868f88a5a9e4939c679b8d418e1ec117d97
Todokanai <bh951201@gmail.com>
2026-05-09 07:41:19 +0900

    mutableIntState 도입

--- app/src/main/java/com/todokanai/busstop_seoul/compose/MainScreen.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/MainScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -35,7 +35,7 @@ fun MainScreen(
     viewModel: MainScreenViewModel = hiltViewModel()
 ){
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
-    var currentTab by remember { mutableStateOf(0)}
+    var currentTab by remember { mutableIntStateOf(0) }
 
     Column {
         Row(
