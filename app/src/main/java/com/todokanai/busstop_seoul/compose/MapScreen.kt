688e26d4b25af5a4d0ddfaadd30da64d492f1976
Todokanai <bh951201@gmail.com>
2026-05-09 07:33:32 +0900

    remove unused import

--- app/src/main/java/com/todokanai/busstop_seoul/compose/MapScreen.kt
+++ app/src/main/java/com/todokanai/busstop_seoul/compose/MapScreen.kt
@@ -3,8 +3,6 @@ package com.todokanai.busstop_seoul.compose
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,7 +11,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
